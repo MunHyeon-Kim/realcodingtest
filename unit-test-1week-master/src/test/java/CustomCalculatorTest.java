13239b876774f2edda320ddd4b14824c6d26061e
MunhyeonKim <qbarche39@naver.com>
2021-04-28 16:23:55 +0900

    Add files via upload

---
+++ unit-test-1week-master/src/test/java/CustomCalculatorTest.java
@@ -0,0 +1,40 @@
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CustomCalculatorTest  {
+
+    private CustomCalculator customCalculator;
+
+    //더하기 테스트를 진행합니다.
+    @Test
+    public void addTest() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.add(10,10);
+        /* 테스트 코드 입력 */
+        assertTrue(result == 20);
+        assertFalse(result == 19);
+    }
+
+    @Test
+    public void testSubtact() {
+        customCalculator = new CustomCalculator();
+        int result = customCalculator.subtract(30,10);
+        assertFalse(result == 19);
+    }
+
+    @Test
+    public void testmulitply() {
+        customCalculator = new CustomCalculator();
+        assertTrue(customCalculator.multiply(2,3) == 6);
+    }
+
+    @Test
+    public void testDivide() {
+        customCalculator = new CustomCalculator();
+        assertTrue(customCalculator.divide(25,5) == 5);
+    }
+
+}
